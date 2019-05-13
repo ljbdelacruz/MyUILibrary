4154fb8885d8735cb17e761487826c01935b25e2
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-05-13 19:51:35 +0800

    updated functions

--- MyUILibrary/UI/UIView/Content2/Content2View.swift
+++ MyUILibrary/UI/UIView/Content2/Content2View.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 
-class Content2View: UIView {
-    @IBOutlet weak var myimage: UIImageView!
-    @IBOutlet weak var mytitle: UILabel!
-    @IBOutlet weak var myurl: UILabel!
-    
+public class Content2View: UIView {
+    public var fontSize:CGFloat=14.0;
+    public var fontColor:String="#7030A0";
+    @IBOutlet public weak var myimage: UIImageView!
+    @IBOutlet public weak var mytitle: UILabel!
+    @IBOutlet public weak var myurl: UILabel!
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
+    override public func draw(_ rect: CGRect) {
         // Drawing code
     }
+    func setAttributeString(data:String)->NSAttributedString{
+        let attrs = [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: self.fontSize),
+            NSAttributedString.Key.foregroundColor : UiUtil.hexStringToUIColor(hex: self.fontColor),
+            NSAttributedString.Key.underlineStyle : 1] as [NSAttributedString.Key : Any]
+        return NSAttributedString(string: data, attributes: attrs)
+    }
+    public var url:String="";
+    public func setupWeb(url:String){
+        self.url=url;
+        self.myurl.attributedText?=self.setAttributeString(data: url)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(Content2View.navigateURL))
+        myurl.isUserInteractionEnabled=true;
+        myurl.addGestureRecognizer(tap);
+        
+    }
+    @objc
+    func navigateURL(sender:UITapGestureRecognizer){
+        //invoke function custom here
+//        if let url = URL(string: self.url) {
+//            UIApplication.shared.openURL(url);
+//        }
+    }
+    
 
 }
